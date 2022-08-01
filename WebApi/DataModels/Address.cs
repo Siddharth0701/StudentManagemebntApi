bc3a6c058bf8625f8149cae81c32eb8255d99cec
Siddharth0701 <siddharth.singh0701@gmail.com>
2022-08-01 14:54:59 +0530

    Added models classes

---
+++ WebApi/DataModels/Address.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace WebApi.DataModels
+{
+    public class Address
+    {
+        public Guid Id { get; set; }
+        public string PhysicalAddress { get; set; }
+        public string PostalAddress { get; set; }
+
+        //navigation prop   
+        public Guid StudentId { get; set; }
+    }
+}
